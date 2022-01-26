e333ae6ad13094c8dd9d573310c38823a75de1bd
Matt <mbrowne108@gmail.com>
2022-01-26 12:30:36 -0500

    commit tree 53f7ce2cc7f2260daf2c5d5600f6b736278c7437

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,9 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "Mousepad", value: 10, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "Pen", value: 15, company_id: 2, dev_id: 1)
+Freebie.create(item_name: "Polo", value: 30, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "Lanyard", value: 1, company_id: 4, dev_id: 3)
 
 puts "Seeding done!"
